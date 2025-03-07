9bd0513748a83b4b102dec533dfff15f833b3760
Scott Furlan <scfurl@users.noreply.github.com>
2025-03-07 07:41:28 -0800

    stuff

--- R/GeneActivityScores.R
+++ R/GeneActivityScores.R
@@ -1,68 +1,147 @@
-# Load required packages
-library(GenomicRanges)
-library(rtracklayer)
-library(TxDb.Hsapiens.UCSC.hg38.knownGene)
-library(org.Hs.eg.db)
-
-# Retrieve gene annotations from TxDb (GRCh38)
-txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
-genesGR <- genes(txdb)
-
-# Annotate genes with symbols using org.Hs.eg.db
-gene_ids <- names(genesGR)
-gene_symbols <- mapIds(org.Hs.eg.db,
-                       keys = gene_ids,
-                       column = "SYMBOL",
-                       keytype = "ENTREZID",
-                       multiVals = "first")
-mcols(genesGR)$symbol <- gene_symbols
-
-# Define a helper function to extend GRanges (similar to ArchR's extendGR)
-extendGR <- function(gr, upstream, downstream) {
-  new_gr <- gr
-  # For genes on the positive strand (or no strand), extend start by upstream and end by downstream.
-  pos_idx <- which(as.character(strand(gr)) %in% c("+", "*"))
-  # For genes on the negative strand, reverse: extend start by downstream and end by upstream.
-  neg_idx <- which(as.character(strand(gr)) == "-")
-
-  if(length(pos_idx) > 0){
-    start(new_gr)[pos_idx] <- pmax(1, start(gr)[pos_idx] - upstream)
-    end(new_gr)[pos_idx] <- end(gr)[pos_idx] + downstream
-  }
-  if(length(neg_idx) > 0){
-    start(new_gr)[neg_idx] <- pmax(1, start(gr)[neg_idx] - downstream)
-    end(new_gr)[neg_idx] <- end(gr)[neg_idx] + upstream
-  }
-  new_gr
-}
-
-# Default parameters from the addGeneScoreMatrix function:
-geneUpstream <- 5000  # extend gene body upstream
-geneDownstream <- 0   # no extension downstream
-useTSS <- FALSE       # use full gene body model (not TSS only)
-extendTSS <- TRUE     # if useTSS were TRUE, extend the TSS
-
-# Extend gene regions according to the defaults:
-if(!useTSS){
-  # When useTSS is FALSE, extend the full gene body.
-  extendedGenes <- extendGR(genesGR, upstream = geneUpstream, downstream = geneDownstream)
-} else {
-  # If using TSS, first shrink the gene to a 1bp region at the TSS (for + strand, start; for - strand, end)
-  extendedGenes <- genesGR
-  pos_idx <- which(as.character(strand(extendedGenes)) %in% c("+", "*"))
-  neg_idx <- which(as.character(strand(extendedGenes)) == "-")
-  if(length(pos_idx) > 0){
-    end(extendedGenes)[pos_idx] <- start(extendedGenes)[pos_idx]
-  }
-  if(length(neg_idx) > 0){
-    start(extendedGenes)[neg_idx] <- end(extendedGenes)[neg_idx]
-  }
-  # Then extend these TSS regions if desired.
-  if(extendTSS){
-    extendedGenes <- extendGR(extendedGenes, upstream = geneUpstream, downstream = geneDownstream)
-  }
-}
-
-# Write the extended gene regions to a BED file
-export(extendedGenes, file.path("/Users/sfurlan/Library/CloudStorage/OneDrive-SharedLibraries-FredHutchinsonCancerCenter/Furlan_Lab - General/experiments/leuklong/integration/fire_atac", "GRCh38_extendedGenes.bed"), format = "BED")
-
+# # Load required packages
+# library(GenomicRanges)
+# library(rtracklayer)
+# library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+# library(org.Hs.eg.db)
+#
+# # Retrieve gene annotations from TxDb (GRCh38)
+# txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+# genesGR <- genes(txdb)
+#
+# # Annotate genes with symbols using org.Hs.eg.db
+# gene_ids <- names(genesGR)
+# gene_symbols <- mapIds(org.Hs.eg.db,
+#                        keys = gene_ids,
+#                        column = "SYMBOL",
+#                        keytype = "ENTREZID",
+#                        multiVals = "first")
+# mcols(genesGR)$symbol <- gene_symbols
+#
+# # Define a helper function to extend GRanges (similar to ArchR's extendGR)
+# extendGR <- function(gr, upstream, downstream) {
+#   new_gr <- gr
+#   # For genes on the positive strand (or no strand), extend start by upstream and end by downstream.
+#   pos_idx <- which(as.character(strand(gr)) %in% c("+", "*"))
+#   # For genes on the negative strand, reverse: extend start by downstream and end by upstream.
+#   neg_idx <- which(as.character(strand(gr)) == "-")
+#
+#   if(length(pos_idx) > 0){
+#     start(new_gr)[pos_idx] <- pmax(1, start(gr)[pos_idx] - upstream)
+#     end(new_gr)[pos_idx] <- end(gr)[pos_idx] + downstream
+#   }
+#   if(length(neg_idx) > 0){
+#     start(new_gr)[neg_idx] <- pmax(1, start(gr)[neg_idx] - downstream)
+#     end(new_gr)[neg_idx] <- end(gr)[neg_idx] + upstream
+#   }
+#   new_gr
+# }
+#
+# # Default parameters from the addGeneScoreMatrix function:
+# geneUpstream <- 5000  # extend gene body upstream
+# geneDownstream <- 0   # no extension downstream
+# useTSS <- FALSE       # use full gene body model (not TSS only)
+# extendTSS <- TRUE     # if useTSS were TRUE, extend the TSS
+#
+# # Extend gene regions according to the defaults:
+# if(!useTSS){
+#   # When useTSS is FALSE, extend the full gene body.
+#   extendedGenes <- extendGR(genesGR, upstream = geneUpstream, downstream = geneDownstream)
+# } else {
+#   # If using TSS, first shrink the gene to a 1bp region at the TSS (for + strand, start; for - strand, end)
+#   extendedGenes <- genesGR
+#   pos_idx <- which(as.character(strand(extendedGenes)) %in% c("+", "*"))
+#   neg_idx <- which(as.character(strand(extendedGenes)) == "-")
+#   if(length(pos_idx) > 0){
+#     end(extendedGenes)[pos_idx] <- start(extendedGenes)[pos_idx]
+#   }
+#   if(length(neg_idx) > 0){
+#     start(extendedGenes)[neg_idx] <- end(extendedGenes)[neg_idx]
+#   }
+#   # Then extend these TSS regions if desired.
+#   if(extendTSS){
+#     extendedGenes <- extendGR(extendedGenes, upstream = geneUpstream, downstream = geneDownstream)
+#   }
+# }
+#
+#
+# # Write the extended gene regions to a BED file
+# export(extendedGenes, file.path("/Users/sfurlan/Library/CloudStorage/OneDrive-SharedLibraries-FredHutchinsonCancerCenter/Furlan_Lab - General/experiments/leuklong/integration/fire_atac", "GRCh38_extendedGenes.bed"), format = "BED")
+#
+# library(GenomicRanges)
+# library(rtracklayer)
+#
+# # Define a default gene model function (adjust as needed)
+# defaultGeneModel <- function(x) {
+#   exp(-abs(x)/5000) + exp(-1)
+# }
+#
+# library(doParallel)
+# library(foreach)
+#
+#
+#
+# calculateFiberSeqFIREScore <- function(fire_file, defaultGeneModel, cores = 8) {
+#   # If fire is a character, assume it's a file path to a BED file and import it.
+#   if (is.character(fire_file)) {
+#     message("Importing FIRE BED file...")
+#     fire <- data.table::fread(fire_file)
+#     colnames(fire) <- c("seqnames", "start", "end", "gene_id", "something", "strand", "score")
+#     fire <- GRanges(fire)
+#     fire$score[fire$score=="."]<-"0"
+#     fire$score <- as.numeric(fire$score)
+#     #fire$symbol <- as.character(extendedGenes$symbol[match(fire$gene_id, extendedGenes$gene_id)])
+#   }
+#
+#   # Ensure the 'fire' GRanges has a metadata column named "score"
+#   if (!"score" %in% colnames(mcols(fire))) {
+#     stop("The FIRE GRanges must have a 'score' column in its metadata.")
+#   }
+#
+#
+#
+#   cl <- makeCluster(cores)
+#   registerDoParallel(cl)
+#
+#   # Parallel loop using foreach
+#   geneScores <- foreach(g = seq_along(fire),
+#                         .combine = c,
+#                         .packages = "GenomicRanges") %dopar% {
+#                           peakIdx <- subjectHits(hits)[queryHits(hits) == g]
+#                           if (length(peakIdx) == 0) {
+#                             0
+#                           } else {
+#                             # Use the center of the gene as reference (using original gene width)
+#                             geneCenter <- start(extendedGenes[g]) + (width(genes[g]) / 2)
+#                             # Calculate peak centers
+#                             peakCenters <- start(fire[peakIdx]) + (width(fire[peakIdx]) / 2)
+#                             distances <- peakCenters - geneCenter
+#
+#                             # Compute weights using the gene model function
+#                             weights <- gene_model(distances)
+#                             # Multiply weights by FIRE score (assumed to be in mcols(fire)$score)
+#                             scores <- mcols(fire[peakIdx])$score * weights
+#                             sum(scores, na.rm = TRUE)
+#                           }
+#                         }
+#
+#   # Stop the cluster
+#   stopCluster(cl)
+#
+#   # Check the result
+#   geneScores
+#   return(geneScores)
+# }
+#
+# # Example usage:
+# # Assume 'genesGR' is your GRanges object for genes with a "symbol" column.
+# # And 'fire_file' is the path to your FIRE BED file.
+# # genesGR <- ... (load or create your gene GRanges)
+# # fire_file <- "path/to/your/FIRE.bed"
+# fire_file <- "/Users/sfurlan/Library/CloudStorage/OneDrive-SharedLibraries-FredHutchinsonCancerResearchCenter/Furlan_Lab - General/experiments/leuklong/integration/fire_atac/LL4_SAdd_GA_sum_scores.bed"
+# #fire <- "/Users/sfurlan/Library/CloudStorage/OneDrive-SharedLibraries-FredHutchinsonCancerResearchCenter/Furlan_Lab - General/experiments/leuklong/integration/fire_atac/LL4_SAdd_GA_sum_scores.bed"
+# geneFIREscores <- calculateFiberSeqFIREScore(fire_file)
+# head(geneFIREscores)
+#
+#
+#
+#
--- debug.R
+++ debug.R
@@ -1,4 +1,105 @@
 roxygen2::roxygenize()
+library(TxDb.Hsapiens.UCSC.hg38.knownGene)
+library(org.Hs.eg.db)
+library(rtracklayer)
+txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
+genesGR <- genes(txdb)
+# # Annotate genes with symbols using org.Hs.eg.db
+gene_ids <- names(genesGR)
+gene_symbols <- mapIds(org.Hs.eg.db,
+                       keys = gene_ids,
+                       column = "SYMBOL",
+                       keytype = "ENTREZID",
+                       multiVals = "first")
+mcols(genesGR)$name <- gene_symbols
+mcols(genesGR)$symbol <- gene_symbols
+#debug(generateExtendedGeneIntervals)
+genesExtended <- generateExtendedGeneIntervals(genesGR, cores = parallel::detectCores())
+extendedDF <- data.frame(
+  seqnames = seqnames(genesExtended),
+  start = start(genesExtended) - 1,  # BED format is 0-based
+  end = end(genesExtended),
+  name = mcols(genesExtended)$symbol,
+  weight = mcols(genesExtended)$geneWeight,
+  strand = strand(genesExtended),
+  tss = mcols(genesExtended)$tss,
+  geneStart = mcols(genesExtended)$geneStart,
+  geneEnd = mcols(genesExtended)$geneEnd
+)
+
+outputFile <- file.path("/Users/sfurlan/Library/CloudStorage/OneDrive-SharedLibraries-FredHutchinsonCancerCenter/Furlan_Lab - General/experiments/leuklong/integration/fire_atac", "GRCh38_extended.bed")
+write.table(
+  extendedDF,
+  file = outputFile,
+  quote = FALSE,
+  sep = "\t",
+  row.names = FALSE,
+  col.names = FALSE
+)
+
+
+bed_df <- data.frame(
+  chrom = seqnames(genesGR),
+  start = start(genesGR) - 1,  # BED format is 0-based for start positions
+  end = end(genesGR),
+  name = mcols(genesGR)$name,
+  strand = as.character(strand(genesGR))
+)
+
+write.table(bed_df, file.path("/Users/sfurlan/Library/CloudStorage/OneDrive-SharedLibraries-FredHutchinsonCancerCenter/Furlan_Lab - General/experiments/leuklong/integration/fire_atac", "GRCh38.bed"), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bed_df <- data.frame(
+  chrom = seqnames(genesGR),
+  start = start(genesGR) - 1,  # BED format is 0-based for start positions
+  end = end(genesGR),
+  name = mcols(genesGR)$name,
+  strand = as.character(strand(genesGR))
+)
+
+write.table(bed_df, file.path("/Users/sfurlan/Library/CloudStorage/OneDrive-SharedLibraries-FredHutchinsonCancerCenter/Furlan_Lab - General/experiments/leuklong/integration/fire_atac", "GRCh38.bed"), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
+#xsdebug(createGeneRegions)
+windows <- createGeneRegions(genes = genesGR, includeChr = paste0("chr", c(1:22, "X")))
+
+windows_df <- data.frame(
+  chrom = seqnames(windows),
+  start = start(windows) - 1,  # BED format is 0-based for start positions
+  end = end(windows),
+  name = mcols(windows)$name,
+  strand = as.character(strand(windows))
+)
+
+write.table(windows_df, file.path("/Users/sfurlan/Library/CloudStorage/OneDrive-SharedLibraries-FredHutchinsonCancerCenter/Furlan_Lab - General/experiments/leuklong/integration/fire_atac", "GRCh38_extended.bed"), sep="\t", quote=FALSE, row.names=FALSE, col.names=FALSE)
+
+# go and run bedtools map on cluster to obtain FIRE score over these extended gene regions
+
+ROOT_DIR2 <- "/Users/sfurlan/Library/CloudStorage/OneDrive-SharedLibraries-FredHutchinsonCancerResearchCenter/Furlan_Lab - General/datasets/Healthy_BM_greenleaf"
+seur <- readRDS(file.path(ROOT_DIR2, "230329_rnaAugmented_seurat.RDS"))
+
+library(Seurat)
+library(flscuts)
+library(monocle3)
+library(patchwork)
+DimPlot(seur, group.by = "SFClassification", cols = seur@misc$colors)
+
+cds <- seurat_to_monocle3(seur)
+
+
 
 data_S1 <- read_json_file("/Volumes/furlan_s/sfurlan/250302_leuklong/pbWGS/LL1_S1/20250302_232026_humanwgs_singleton/outputs.json")
 data_S18 <- read_json_file("/Volumes/furlan_s/sfurlan/250302_leuklong/pbWGS/LL2_S18/20250302_232206_humanwgs_singleton/outputs.json")
@@ -7,11 +108,67 @@ data_SAdd <- read_json_file("/Volumes/furlan_s/sfurlan/250302_leuklong/pbWGS/LL4
 
 file <- "/Users/sfurlan/Library/CloudStorage/OneDrive-SharedLibraries-FredHutchinsonCancerResearchCenter/Furlan_Lab - General/experiments/leuklong/250302_SCRI/res/LL4_SAdd.small_variants.ann.txt"
 
-plot_snpEff(file)
+DATA_DIR <- "/Users/sfurlan/Library/CloudStorage/OneDrive-SharedLibraries-FredHutchinsonCancerResearchCenter/Furlan_Lab - General/experiments/leuklong/integration"
+files <- list.files(file.path(DATA_DIR, "fire_atac"))
+file <- files[grepl("GA", files)]
+files <- file.path(DATA_DIR, "fire_atac", file)
+samps <- c( "EP300-ZNF384_ALL", "ETV6-RUNX1_ALL", "APML","NUP-NSD1_AML")
 
-anns <- vroom(file, guess_max = 1000000)
-flt3 <- anns[(anns$CHROM %in% "chr13" & anns$POS > 28003274 & anns$POS < 28100592),]
-wt1 <- anns[(anns$CHROM %in% "chr11" & anns$POS > 32396330 & anns$POS <  32396400),]
+make_matrix_from_fs_gas <- function(files, samps=NULL){
+ if(is.null(samps)){
+   samps <- basename(files)
+ }
+ if(length(samps)!=length(files)) stop("length of files vector and samps should be the same")
+ bed_data <- lapply(1:length(files), function(x) {
+   dat <- data.table::fread(files[x], header = F)
+   dat$samp <- samps[x]
+   colnames(dat)<-c("seqnames", "start", "end", "gene", "strand", "score", "samp")
+   dat$score[dat$score=="."] <- 0
+   dat$score[dat$score < 0] <- 0
+   gas_vector <- dat$score
+   gas_vector <- as.numeric(gas_vector)
+   names(gas_vector) <- dat$gene
+   gas_vector
+ })
+ mat <- Matrix(do.call(cbind, bed_data), sparse = T)
+ colnames(mat)<-samps
+ mat
+}
+
+#debug(make_matrix_from_fs_gas)
+mat <- make_matrix_from_fs_gas(files, samps)
+
+
+
+
+fs <- SummarizedExperiment(assays = as.matrix(mat), colData = DataFrame(name = colnames(mat), type = colnames(mat), row.names = colnames(mat)))
+
+PCs <- 25 #Number of PCs for clustering
+n_top <- c(3000,2500,2500) #number of features
+rownames(seur[["umap"]]@cell.embeddings)
+
+
+rna<-iterative_LSI(cds, num_dim = PCs, num_features = n_top, resolution = c(2e-4, 9e-4, 1e-3), verbose = T)
+rna <- reduce_dimension(rna, reduction_method = "UMAP", preprocess_method = "LSI", verbose = T, umap.min_dist = 0.45, umap.n_neighbors = 55,umap.metric = "euclidean")
+
+p1 <- plot_cells(rna, color_cells_by = "SFClassification")+scale_color_manual(values = seur@misc$colors)
+
+projection <- project_data(projector = rna, projectee = fs, reduced_dim = "LSI", embedding = "UMAP", features = "annotation-based", projectee_label_col = "type", make_pseudo_single_cells = T, n = 1000, force = T)
+
+
+p<-as.data.frame(do.call(rbind, projection[2:1]))
+cols2 <- pals::glasbey(21)[3:(3+dim(fs)[2])]
+names(cols2)<-colData(fs)$name
+cols2 <- c(cols2, "single_cell_reference"="lightgray")
+
+p2<-ggplot(p, aes(x=UMAP1, y=UMAP2, color=projectee_labels))+geom_point(size=0.2, alpha=0.5)+ guides(colour = guide_legend(override.aes = list(size=5)))+monocle3:::monocle_theme_opts()+scale_color_manual(values=cols2 )
+p1+p2
+
+
+fs
+
+
+
 
 muts <- as.data.frame(rbind(wt1[1,], flt3[51,])) %>% as.data.frame()
 write.csv(muts, "~/Desktop/muts.csv")
@@ -144,3 +301,10 @@ gt_tbl <- gt::gt(tabb)
 write.csv(tabb, "~/Desktop/pharmcat.csv")
 # Show the gt Table
 gt_tbl
+
+
+
+BiocManager::install("chromVAR")
+devtools::install_github("GreenleafLab/ArchR", ref="master", repos = BiocManager::repositories())
+library(ArchR)
+ArchR::installExtraPackages()
