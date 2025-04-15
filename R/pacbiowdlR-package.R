8de221dbe72b3bdb8e3ae0bd35c605da7b6f33a4
Scott Furlan <scfurl@users.noreply.github.com>
2025-04-15 13:07:34 -0700

    clening up

---
+++ R/pacbiowdlR-package.R
@@ -0,0 +1,8 @@
+#' pacbiowdlR: Plotting DNA Copy Number Alterations from PacBio WDL Data
+#'
+#' The **pacbiowdlR** package provides functions for visualizing copy number alterations (CNAs)
+#' from PacBio WDL pipelines. Its functions leverage **ggplot2** to create customizable plots.
+#'
+#' @docType _PACKAGE
+#' @name pacbiowdlR-package
+NULL
